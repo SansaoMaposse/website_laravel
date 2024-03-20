94d1aebf224d2c6d271505f11b8a8e25bf00d6c6
Sansão Maposse <Sansaomaposse1@gmail.com>
2024-03-20 22:49:54 +0200

    Confiração do templete Html, trazendo para o  projecto, integarndo com laravel.

--- website.com/routes/web.php
+++ website.com/routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/', [HomeController::class, 'home']);
 Route::get('login', [AuthController::class, 'login']);
 Route::get('register', [AuthController::class, 'register']);
 Route::get('forgot-password', [AuthController::class, 'forgot']);
