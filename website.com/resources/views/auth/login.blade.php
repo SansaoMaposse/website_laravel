50160d5a980c8100a8ef02c282607025abd88a15
Sansão Maposse <Sansaomaposse1@gmail.com>
2024-03-21 00:25:38 +0200

    Registro de Usuário e Validação de E-mail Único.

--- website.com/resources/views/auth/login.blade.php
+++ website.com/resources/views/auth/login.blade.php
@@ -43,9 +43,9 @@
             <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
               <div class="d-flex justify-content-center py-4">
-                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                <a href="{{url('')}}" class="logo d-flex align-items-center w-auto">
                   <img src="{{url('assets/img/logo.png')}}" alt="">
-                  <span class="d-none d-lg-block">NiceAdmin</span>
+                  <span class="d-none d-lg-block">Blog</span>
                 </a>
               </div><!-- End Logo -->
 
@@ -57,7 +57,7 @@
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                     <p class="text-center small">Enter your username & password to login</p>
                   </div>
-
+                   @include('layouts._message')
                   <form class="row g-3 needs-validation" novalidate>
 
                     <div class="col-12">
